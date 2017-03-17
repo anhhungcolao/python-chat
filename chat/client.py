ac1c792d3efcffca396d583ebd40f8287700a092
anhhungcolao <=>
2017-03-17 10:05:53 +0700

    huhuhu

--- chat/client.py
+++ chat/client.py
@@ -1,119 +1,44 @@
-# -*- encoding: utf-8 -*-
-
-import os
-import socket
 import sys
+import socket
+import time
 import select
 
-from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_PSS
-from Crypto.Hash import SHA
-
-from communication import send, receive
-
-
-class ChatClient(object):
-
-    def __init__(self, name, host='127.0.0.1', port=3490):
-        self.name = name
-        # Quit flag
-        self.flag = False
-        self.port = int(port)
-        self.host = host
-
-        # Initial prompt
-        self.prompt = '[' + '@'.join((name, socket.gethostname().split('.')[0])) + ']> '
-
-        client_privkey = RSA.generate(4096, os.urandom)
-        client_pubkey = client_privkey.publickey()
-
-        self.decryptor = client_privkey
-
-        # Connect to server at port
+class Client:
+    def __init__(self, server_address = '127.0.0.1', server_port = 1234):
+        self.running  = True
         try:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.connect((host, self.port))
-            print 'Connected to chat server %s:%d' % (self.host, self.port)
-            # Send my pubkey...
-            send(self.sock, client_pubkey.exportKey())
-            server_pubkey = receive(self.sock)
-
-            self.encryptor = RSA.importKey(server_pubkey)
-
-            # Send my name...
-            send(self.sock, 'NAME: ' + self.name)
-            data = receive(self.sock)
-
-            # Contains client address, set it
-            addr = data.split('CLIENT: ')[1]
-            self.prompt = '[' + '@'.join((self.name, addr)) + ']> '
-
+            self.sock  = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+            self.sock.connect((server_address,int(server_port)))
         except socket.error:
-            print 'Could not connect to chat server @%d' % self.port
-            sys.exit(1)
+            print 'error'
+            self.sock.close();
+            self.running = False
 
-    def cmdloop(self):
-        while not self.flag:
-            try:
-                sys.stdout.write(self.prompt)
-                sys.stdout.flush()
+    def process(self, x):
+        if (x == sys.stdin):
+            data = sys.stdin.readline()
+            self.send(data)
+        elif x == self.sock:
+            data = x.recv(100)
+            print >>sys.stderr , data,
 
-                # Wait for input from stdin & socket
-                inputready, outputready, exceptrdy = select.select([0, self.sock], [], [])
+    def send(self,data):
+        if data:
+            data = sys.argv[1] + ': ' + data
+            self.sock.sendall(data)
 
-                for i in inputready:
-                    if i == 0:
-                        # grab message
-                        data = sys.stdin.readline().strip()
-
-                        try:
-                            # encrypt
-                            data = self.encryptor.encrypt(data, 0)
-                            data = data[0]
-
-                            # append signature
-                            signkey = self.decryptor
-                            message_hash = SHA.new()
-                            message_hash.update(data)
-
-                            signer = PKCS1_PSS.new(signkey)
-                            signature = signer.sign(message_hash)
-                            data = '%s#^[[%s' % (data, signature)
-
-                        except ValueError:
-                            print 'Too large text, cannot encrypt, not sending.'
-                            data = None
-
-                        if data:
-                            send(self.sock, data)
-
-                    elif i == self.sock:
-                        data = receive(self.sock)
-
-                        if not data:
-                            print 'Shutting down.'
-                            self.flag = True
-                            break
-
-                        else:
-                            if 'PLAIN:' in data:
-                                data = data.strip('PLAIN:').strip()
-                            else:
-                                data = self.decryptor.decrypt(data)
-
-                            sys.stdout.write(data + '\n')
-                            sys.stdout.flush()
-
-            except KeyboardInterrupt:
-                print 'Interrupted.'
-                self.sock.close()
-                break
+    def main(self):
+        self.input = [self.sock,sys.stdin]
+        self.output = []
+        while self.running:
+            inputready,self.outputready,exceptional = select.select(self.input,self.output,[])
+            for s in inputready:
+                self.process(s)
+            
 
 
-if __name__ == "__main__":
-
-    if len(sys.argv) < 3:
-        sys.exit('Usage: %s username host portno' % sys.argv[0])
-
-    client = ChatClient(sys.argv[1], sys.argv[2], int(sys.argv[3]))
-    client.cmdloop()
+if __name__ == '__main__':
+    if (len(sys.argv) < 4):
+        print >> sys.stderr, 'Usage python client.py user ip port'
+    else:
+        Client(sys.argv[2],sys.argv[3]).main()
--- chat/server.py
+++ chat/server.py
@@ -1,193 +1,80 @@
-# -*- encoding: utf-8 -*-
-
-import os
-import select
 import socket
 import sys
-import signal
-from time import sleep
+import select
+import time
 
-from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_PSS
-from Crypto.Hash import SHA
-
-from communication import send, receive
-
-
-class ChatServer(object):
-
-    def __init__(self, address='127.0.0.1', port=3490):
-        self.clients = 0
-
-        # Client map
-        self.clientmap = {}
-
-        # Output socket list
-        self.outputs = []
-
+class ChatServer:
+    def __init__(self, server_address = '127.0.0.1', server_port = 1234):
+        self.clients = []
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((address, int(port)))
-
-        print 'Generating RSA keys ...'
-        self.server_privkey = RSA.generate(4096, os.urandom)
-        self.server_pubkey = self.server_privkey.publickey()
-
-        print 'Listening to port', port, '...'
+        self.server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        self.server.bind((server_address,int(server_port)))
         self.server.listen(5)
 
-        # Trap keyboard interrupts
-        signal.signal(signal.SIGINT, self.sighandler)
+    def process(self, x):
+        if (x == self.server):
+            #co 1 thang client ket noi toi server
+            client, address = self.server.accept()
+            client.setblocking(0)
+            self.clients.append(client)
+            self.inputs.append(client)
+            print >> sys.stderr, 'new connection from' , client, address
 
-    def sighandler(self, signum, frame):
-        # Close the server
-        print 'Shutting down server...'
-
-        # Close existing client sockets
-        for o in self.outputs:
-            o.close()
-
-        self.server.close()
-
-    def getname(self, client):
-        # Return the printable name of the
-        # client, given its socket...
-        info = self.clientmap[client]
-        host, name = info[0][0], info[1]
-        return '@'.join((name, host))
-
-    def get_just_name(self, client):
-        return self.clientmap[client][1]
-
-    def send_encrypted(self, to_who, message, name):
-        try:
-            encryptor = self.clientmap[to_who][2]
-            msg = encryptor.encrypt(message, 0)
-            send(to_who, msg)
-
-        except IOError:
-            send(to_who, 'PLAIN: cannot find public key for: %s' % name)
-
-    def verify_signature(self, client, message, signature):
-        try:
-            key = self.clientmap[client][2]
-            msg_hash = SHA.new()
-            msg_hash.update(message)
-
-            verifier = PKCS1_PSS.new(key)
-            return verifier.verify(msg_hash, signature)
-
-        except IOError:
-            return False
-
-    def serve(self):
-        inputs = [self.server, sys.stdin]
-        self.outputs = []
-
-        running = 1
-
-        while running:
+        elif x == sys.stdin:
+            #ai do dang dung cham vao server
+            self.running = False
+            print >> sys.stderr, 'Dont touch anything to server pls'
+            self.server.close()
+        
+        else:
+            #x gui den server 1 request
             try:
-                inputready, outputready, exceptready = select.select(inputs, self.outputs, [])
-
-            except select.error:
-                break
-
-            except socket.error:
-                break
-
-            for s in inputready:
-                if s == self.server:
-                    # handle the server socket
-                    client, address = self.server.accept()
-                    print 'chatserver: got connection %d from %s' % (client.fileno(), address)
-                    # Get client public key and send our public key
-                    pubkey = RSA.importKey(receive(client))
-                    send(client, self.server_pubkey.exportKey())
-
-                    # Read the login name
-                    cname = receive(client).split('NAME: ')[1]
-
-                    # Compute client name and send back
-                    self.clients += 1
-                    send(client, 'CLIENT: ' + str(address[0]))
-                    inputs.append(client)
-
-                    self.clientmap[client] = (address, cname, pubkey)
-
-                    # Send joining information to other clients
-                    msg = '\n(Connected: New client (%d) from %s)' % (self.clients, self.getname(client))
-
-                    for o in self.outputs:
-                        try:
-                            self.send_encrypted(o, msg, self.get_just_name(o))
-
-                        except socket.error:
-                            self.outputs.remove(o)
-                            inputs.remove(o)
-
-                    self.outputs.append(client)
-
-                elif s == sys.stdin:
-                    # handle standard input
-                    sys.stdin.readline()
-                    running = 0
+                data = x.recv(1024)
+                # print >> sys.stderr , 'server revieved: ' , data
+                if data:
+                    for o in self.clients:
+                        if o!=x:
+                            o.send(data)
                 else:
+                    self.inputs.remove(x)
+                    if s in self.clients:
+                        self.clients.remove(x)
+                    x.close()
+                    print >> sys.stderr , 'Closing ', address , '!'
+            except socket.error:
+                x.close()
+                print>>sys.stderr,'Client',x, 'is disconnect'   
+                # self.running = False
 
-                    # handle all other sockets
-                    try:
-                        data = receive(s)
 
-                        if data:
-                            dataparts = data.split('#^[[')
-                            signature = dataparts[1]
-                            data = dataparts[0]
-
-                            verified = self.verify_signature(s, data, signature)
-                            data = self.server_privkey.decrypt(data)
-
-                            if data != '\x00':
-                                if verified:
-                                    data = '%s [OK]' % data
-
-                                else:
-                                    data = '%s [Not verified]' % data
-
-                                # Send as new client's message...
-                                msg = '\n# [' + self.getname(s) + ']>> ' + data
-
-                                # Send data to all except ourselves
-
-                                for o in self.outputs:
-                                    if o != s:
-                                        self.send_encrypted(o, msg, self.get_just_name(s))
-
-                        else:
-
-                            print 'Chatserver: Client %d hung up' % s.fileno()
-                            self.clients -= 1
-                            s.close()
-                            inputs.remove(s)
-                            self.outputs.remove(s)
-
-                            # Send client leaving information to others
-                            msg = '\n(Hung up: Client from %s)' % self.getname(s)
-
-                            for o in self.outputs:
-                                self.send_encrypted(o, msg, self.get_just_name(o))
-
-                    except socket.error:
-                        # Remove
-                        inputs.remove(s)
-                        self.outputs.remove(s)
-
-            sleep(0.1)
+    def main(self):
+        self.inputs = [self.server, sys.stdin]
+        self.running = 1
+        print >> sys.stderr , 'Waiting for a connection....!'
+        while self.running:
+            try:
+                inputready, outputready, exceptionready = select.select(self.inputs, self.clients, [])
+            except select.error:
+                self.running = False
+                self.server.close()
+            except socket.error:
+                self.running = False
+                self.server.close()
+            for s in inputready:
+                self.process(s)
+            for s in exceptionready:
+                if s in inputready: 
+                    self.inputs.remove(s)
+                if s in self.clients:
+                    self.clients.remove(s)
+            for s in exceptionready:
+                self.inputs.remove(s)
+                self.clients.remove(s)
 
         self.server.close()
 
-if __name__ == "__main__":
-
-    if len(sys.argv) < 3:
-        sys.exit('Usage: %s listen_ip listen_port' % sys.argv[0])
-
-    ChatServer(sys.argv[1], sys.argv[2]).serve()
+if __name__ == '__main__':
+    if (len(sys.argv) < 3):
+        print >> sys.stderr, 'Usage python ',sys.argv[0],' listen_ip listen_add'    
+    else:
+        ChatServer(sys.argv[1], sys.argv[2]).main()
